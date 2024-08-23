6ef7aaafc9b18d1fcfbdbbd8e2642e3e2ebea779
srinihashi <srinivas@hashicorp.com>
2024-08-22 19:21:36 -0500

    update

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "ec2" {
+  ami = "ami-0fee185d37202108d"
+  instance_type = "t2.micro"
+  key_name = "aws-ssh-key"
+  security_groups = ["ssh-from-my-mac"]
+
+  tags = {
+    Name = "FROM-AWS-GITHUB-RUNNER"
+  }
+}
