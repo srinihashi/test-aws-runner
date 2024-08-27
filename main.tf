3b413aaf898f04eaaa30eb5751fbdda10b493cd8
srinihashi <srinivas@hashicorp.com>
2024-08-27 16:49:21 -0500

    use git secret and run on TFC

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+# The configuration for the `remote` backend.
+terraform {
+  backend "remote" {
+    organization = "srini-hashi"
+    workspaces {
+      name = "sg-test"
+     }
+   }
+ }
+#     # An example resource that does nothing.
+#     resource "null_resource" "example" {
+#       triggers = {
+#         value = "A example resource that does nothing!"
+#       }
+#     }
 // Made upate to trigger pipeline
 resource "aws_instance" "ec2" {
   ami = "ami-0fee185d37202108d"
